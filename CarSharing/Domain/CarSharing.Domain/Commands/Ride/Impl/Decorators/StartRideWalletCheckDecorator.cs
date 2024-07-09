44ed6dbf5dca2bfcabe7c5f00434b6e17871c266
Raman Laureniuk <roman.lavreniuk@gmail.com>
2024-07-09 19:55:27 +0200

    Moved wallet check to decorator.

---
+++ CarSharing/Domain/CarSharing.Domain/Commands/Ride/Impl/Decorators/StartRideWalletCheckDecorator.cs
@@ -0,0 +1,57 @@
+﻿namespace CarSharing.Domain.Commands.Ride.Impl.Decorators
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using CarSharing.Domain.Commands.Ride;
+    using CarSharing.Domain.Commands.Wallet;
+    using CarSharing.Domain.Dto.Ride.Request;
+    using CarSharing.Domain.Dto.Ride.Response;
+    using CarSharing.Domain.Dto.Wallet.Request;
+    using CarSharing.Domain.Dto.Wallet.Response;
+
+    internal class StartRideWalletCheckDecorator : IStartRideCommandAsync
+    {
+        private readonly IGetWalletsCommandAsync _getWalletsCommand;
+        private readonly IStartRideCommandAsync _decoratee;
+
+        public StartRideWalletCheckDecorator(IGetWalletsCommandAsync getWalletsCommand,
+            IStartRideCommandAsync decoratee)
+        {
+            _getWalletsCommand = getWalletsCommand ?? throw new ArgumentNullException(nameof(getWalletsCommand));
+            _decoratee = decoratee ?? throw new ArgumentNullException(nameof(decoratee));
+        }
+        
+        public async Task<StartRideResponseDto> ExecuteAsync(StartRideRequestDto request)
+        {
+            await ThrowIfWalletIsNotClients(request.ClientId, request.WalletId);
+
+            return await _decoratee.ExecuteAsync(request);
+        }
+
+        private async Task<bool> IsClientsWallet(Guid clientId, int walletId)
+        {
+            GetWalletsRequestDto request = new GetWalletsRequestDto()
+            {
+                ClientId = clientId
+            };
+
+            GetWalletsResponseDto walletsResponse = await _getWalletsCommand.ExecuteAsync(request);
+
+            return walletsResponse
+                ?.Wallets
+                ?.Select(x => x.WalletId)
+                .Contains(walletId) ?? false;
+        }
+
+        private async Task ThrowIfWalletIsNotClients(Guid clientId, int walletId)
+        {
+            bool isClientsWallet = await IsClientsWallet(clientId, walletId);
+
+            if (!isClientsWallet)
+            {
+                throw new ArgumentException($"Wallet {walletId} doesn't belong to client {clientId}.");
+            }
+        }
+    }
+}
--- CarSharing/Domain/CarSharing.Domain/Commands/Ride/Impl/StartRideCommandAsync.cs
+++ CarSharing/Domain/CarSharing.Domain/Commands/Ride/Impl/StartRideCommandAsync.cs
@@ -5,11 +5,8 @@
     using System.Linq;
     using System.Threading.Tasks;
     using CarSharing.Domain.Commands.Ride;
-    using CarSharing.Domain.Commands.Wallet;
     using CarSharing.Domain.Dto.Ride.Request;
     using CarSharing.Domain.Dto.Ride.Response;
-    using CarSharing.Domain.Dto.Wallet.Request;
-    using CarSharing.Domain.Dto.Wallet.Response;
     using CarSharing.Domain.Entities;
     using CarSharing.Domain.Enums.Ride;
     using CarSharing.Domain.Exceptions.Ride;
@@ -19,13 +16,10 @@
 
     internal class StartRideCommandAsync : IStartRideCommandAsync
     {
-        private readonly IGetWalletsCommandAsync _getWalletsCommand;
         private readonly IRideRepositoryFactory _repoFactory;
 
-        public StartRideCommandAsync(IGetWalletsCommandAsync getWalletsCommand,
-            IRideRepositoryFactory repoFactory)
+        public StartRideCommandAsync(IRideRepositoryFactory repoFactory)
         {
-            _getWalletsCommand = getWalletsCommand ?? throw new ArgumentNullException(nameof(getWalletsCommand));
             _repoFactory = repoFactory ?? throw new ArgumentNullException(nameof(repoFactory));
         }
 
@@ -35,8 +29,6 @@
             {
                 throw new ArgumentNullException(nameof(request));
             }
-
-            await ThrowIfWalletIsNotClients(request.ClientId, request.WalletId);  // TODO: move to decorator.
 
             using (IRideRepository repo = _repoFactory.CreateRepository())
             {
@@ -57,31 +49,6 @@
                     RideId = ride.Id,
                     StartDateUtc = ride.StartDateUtc
                 };
-            }
-        }
-
-        private async Task<bool> IsClientsWallet(Guid clientId, int walletId)
-        {
-            GetWalletsRequestDto request = new GetWalletsRequestDto()
-            {
-                ClientId = clientId
-            };
-
-            GetWalletsResponseDto walletsResponse = await _getWalletsCommand.ExecuteAsync(request);
-
-            return walletsResponse
-                ?.Wallets
-                ?.Select(x => x.WalletId)
-                .Contains(walletId) ?? false;
-        }
-
-        private async Task ThrowIfWalletIsNotClients(Guid clientId, int walletId)
-        {
-            bool isClientsWallet = await IsClientsWallet(clientId, walletId);
-
-            if (!isClientsWallet)
-            {
-                throw new ArgumentException($"Wallet {walletId} doesn't belong to client {clientId}.");
             }
         }
     }
