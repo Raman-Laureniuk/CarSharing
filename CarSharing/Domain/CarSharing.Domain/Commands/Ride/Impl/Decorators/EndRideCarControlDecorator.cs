7121bff763704ca5d87cc86a34a143cc926add86
Raman Laureniuk <roman.lavreniuk@gmail.com>
2024-07-09 18:51:16 +0200

    Car control implementation.

---
+++ CarSharing/Domain/CarSharing.Domain/Commands/Ride/Impl/Decorators/EndRideCarControlDecorator.cs
@@ -0,0 +1,55 @@
+﻿namespace CarSharing.Domain.Commands.Ride.Impl.Decorators
+{
+    using System;
+    using System.Threading.Tasks;
+    using CarSharing.Domain.Dto.CarControl.Request;
+    using CarSharing.Domain.Dto.Ride.Request;
+    using CarSharing.Domain.Dto.Ride.Response;
+    using CarSharing.Domain.Entities;
+    using CarSharing.Domain.Providers.CarControl;
+    using CarSharing.Domain.Repository.Ride;
+    using CarSharing.Domain.RepositoryFactory.Ride;
+
+    internal class EndRideCarControlDecorator : IEndRideCommandAsync
+    {
+        private readonly IRideRepositoryFactory _repoFactory;
+        private readonly ICarControlProvider _carControlProvider;
+        private readonly IEndRideCommandAsync _decoratee;
+
+        public EndRideCarControlDecorator(IRideRepositoryFactory repoFactory,
+            ICarControlProvider carControlProvider,
+            IEndRideCommandAsync decoratee)
+        {
+            _repoFactory = repoFactory ?? throw new ArgumentNullException(nameof(repoFactory));
+            _carControlProvider = carControlProvider ?? throw new ArgumentNullException(nameof(carControlProvider));
+            _decoratee = decoratee ?? throw new ArgumentNullException(nameof(decoratee));
+        }
+
+        public async Task<EndRideResponseDto> ExecuteAsync(EndRideRequestDto request)
+        {
+            Guid carId = await GetCarIdAsync(request.RideId);
+            
+            try
+            {
+                return await _decoratee.ExecuteAsync(request);
+            }
+            finally
+            {
+                await _carControlProvider.LockCarAsync(new LockRequestDto()
+                {
+                    CarId = carId
+                });
+            }
+        }
+
+        private async Task<Guid> GetCarIdAsync(int rideId)
+        {
+            using (IRideRepository repo = _repoFactory.CreateRepository())
+            {
+                Ride ride = await repo.GetByIdAsync(rideId);
+
+                return ride.CarId;
+            }
+        }
+    }
+}
