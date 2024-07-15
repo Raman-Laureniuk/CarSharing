7ce5a1acb019fd48ea515ad029973c45c50a2e92
Raman Laureniuk <roman.lavreniuk@gmail.com>
2024-07-15 12:54:09 +0200

    Check that client is active before start riding.

---
+++ CarSharing/Domain/CarSharing.Domain/Commands/Ride/Start/Impl/Decorators/StartRideClientActiveCheckDecorator.cs
@@ -0,0 +1,55 @@
+﻿namespace CarSharing.Domain.Commands.Ride.Start.Impl.Decorators
+{
+    using System;
+    using System.Threading.Tasks;
+    using CarSharing.Domain.Commands.Ride.Start;
+    using CarSharing.Domain.Dto.Ride.Request;
+    using CarSharing.Domain.Dto.Ride.Response;
+    using CarSharing.Domain.Entities;
+    using CarSharing.Domain.Exceptions.Ride;
+    using CarSharing.Domain.Repository.Client;
+    using CarSharing.Domain.RepositoryFactory.Client;
+
+    internal class StartRideClientActiveCheckDecorator : IStartRideCommandAsync
+    {
+        private readonly IClientRepositoryFactory _repoFactory;
+        private readonly IStartRideCommandAsync _decoratee;
+
+        public StartRideClientActiveCheckDecorator(IClientRepositoryFactory repoFactory,
+            IStartRideCommandAsync decoratee)
+        {
+            _repoFactory = repoFactory ?? throw new ArgumentNullException(nameof(repoFactory));
+            _decoratee = decoratee ?? throw new ArgumentNullException(nameof(decoratee));
+        }
+
+        public async Task<StartRideResponseDto> ExecuteAsync(StartRideRequestDto request)
+        {
+            if (request != null)
+            {
+                await ThrowIfClientNotActive(request.ClientId);
+            }
+
+            return await _decoratee.ExecuteAsync(request);
+        }
+
+        private async Task ThrowIfClientNotActive(Guid clientId)
+        {
+            bool isClientActive = await IsClientActive(clientId);
+
+            if (!isClientActive)
+            {
+                throw new ClientNotActiveException($"Client {clientId} is not active.");
+            }
+        }
+
+        private async Task<bool> IsClientActive(Guid clientId)
+        {
+            using (IClientRepository repo = _repoFactory.CreateRepository())
+            {
+                Client client = await repo.GetByIdAsync(clientId);
+
+                return client.IsActive;
+            }
+        }
+    }
+}
---
+++ CarSharing/Domain/CarSharing.Domain/Exceptions/Ride/ClientNotActiveException.cs
@@ -0,0 +1,22 @@
+﻿namespace CarSharing.Domain.Exceptions.Ride
+{
+    using System;
+
+    internal class ClientNotActiveException : ApplicationException
+    {
+        public ClientNotActiveException()
+            : base()
+        {
+        }
+
+        public ClientNotActiveException(string message)
+            : base(message)
+        {
+        }
+
+        public ClientNotActiveException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
