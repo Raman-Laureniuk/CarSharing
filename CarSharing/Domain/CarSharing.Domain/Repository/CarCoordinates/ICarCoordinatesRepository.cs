c50172e0ff1cf56ddc2206ae0a828b9abd38aba8
Raman Laureniuk <roman.lavreniuk@gmail.com>
2024-07-24 20:27:45 +0200

    Upsert CarCoordinates.

--- CarSharing/Domain/CarSharing.Domain/Repository/CarCoordinates/ICarCoordinatesRepository.cs
+++ CarSharing/Domain/CarSharing.Domain/Repository/CarCoordinates/ICarCoordinatesRepository.cs
@@ -1,9 +1,11 @@
 ﻿namespace CarSharing.Domain.Repository.CarCoordinates
 {
+    using System.Threading.Tasks;
     using CarSharing.Domain.Entities;
     using CarSharing.Domain.Repository.Base;
 
     public interface ICarCoordinatesRepository : IRepository<CarCoordinates>
     {
+        Task UpsertAsync(CarCoordinates item, bool commit = false);
     }
 }
--- CarSharing/Infrastructure/CarSharing.Repository.Entity/Repository/CarCoordinates/Impl/CarCoordinatesRespository.cs
+++ CarSharing/Infrastructure/CarSharing.Repository.Entity/Repository/CarCoordinates/Impl/CarCoordinatesRespository.cs
@@ -1,5 +1,7 @@
 ﻿namespace CarSharing.Repository.Entity.Repository.CarCoordinates.Impl
 {
+    using System;
+    using System.Threading.Tasks;
     using CarSharing.Domain.Entities;
     using CarSharing.Domain.Repository.CarCoordinates;
     using CarSharing.Repository.Entity.Repository.Base.Impl;
@@ -10,6 +12,31 @@
         public CarCoordinatesRespository(IDbContextFactory<CarSharingContext> dbContextFactory)
             : base(dbContextFactory)
         {
+        }
+
+        public async Task UpsertAsync(CarCoordinates item, bool commit = false)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            CarCoordinates existingItem = await _context.Set<CarCoordinates>().FindAsync(item.CarId);
+
+            if (existingItem == null)
+            {
+                _context.Set<CarCoordinates>().Add(item);
+            }
+            else
+            {
+                _context.Set<CarCoordinates>().Entry(existingItem).State = EntityState.Detached;
+                _context.Set<CarCoordinates>().Update(item);
+            }
+
+            if (commit)
+            {
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
