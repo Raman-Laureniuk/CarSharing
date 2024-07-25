713788d5c016e0c6f53a00fe970845a3ca650a96
Raman Laureniuk <roman.lavreniuk@gmail.com>
2024-07-25 19:05:13 +0200

    Created CarSharing.Domain.UnitTests project. ActivateClientCommandAsyncTests.

---
+++ CarSharing/Tests/UnitTests/CarSharing.Domain.UnitTests/Commands/Client/ActivateClientCommandAsyncTests.cs
@@ -0,0 +1,81 @@
+﻿namespace CarSharing.Domain.UnitTests.Commands.Client
+{
+    using System;
+    using System.Threading.Tasks;
+    using CarSharing.Domain.Commands.Client.Impl;
+    using CarSharing.Domain.Dto.Client.Request;
+    using CarSharing.Domain.Dto.Client.Response;
+    using CarSharing.Domain.Entities;
+    using CarSharing.Domain.Repository.Client;
+    using CarSharing.Domain.RepositoryFactory.Client;
+    using FakeItEasy;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ActivateClientCommandAsyncTests
+    {
+        [TestMethod]
+        public async Task ExecuteAsync_NullRequest_ExceptionThrown()
+        {
+            // Arrange
+            IClientRepositoryFactory repoFactory = A.Fake<IClientRepositoryFactory>();
+            ActivateClientCommandAsync command = new ActivateClientCommandAsync(repoFactory);
+            ActivateClientRequestDto request = null;
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentNullException>(() => command.ExecuteAsync(request));
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsync_ClientNotFound_ExceptionThrown()
+        {
+            // Arrange
+            IClientRepository repo = A.Fake<IClientRepository>();
+            A.CallTo(() => repo.GetByIdAsync(A<object>._)).Returns<Client>(null);
+            
+            IClientRepositoryFactory repoFactory = A.Fake<IClientRepositoryFactory>();
+            A.CallTo(() => repoFactory.CreateRepository()).Returns(repo);
+            
+            ActivateClientCommandAsync command = new ActivateClientCommandAsync(repoFactory);
+            
+            ActivateClientRequestDto request = new ActivateClientRequestDto()
+            {
+                ClientId = Guid.Empty
+            };
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<ArgumentOutOfRangeException>(() => command.ExecuteAsync(request));
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsync_ClientActivated()
+        {
+            // Arrange
+            Client client = new Client()
+            {
+                IsActive = false
+            };
+            
+            IClientRepository repo = A.Fake<IClientRepository>();
+            A.CallTo(() => repo.GetByIdAsync(A<object>._)).Returns(client);
+
+            IClientRepositoryFactory repoFactory = A.Fake<IClientRepositoryFactory>();
+            A.CallTo(() => repoFactory.CreateRepository()).Returns(repo);
+
+            ActivateClientCommandAsync command = new ActivateClientCommandAsync(repoFactory);
+
+            ActivateClientRequestDto request = new ActivateClientRequestDto()
+            {
+                ClientId = Guid.Empty
+            };
+
+            // Act
+            ActivateClientResponseDto response = await command.ExecuteAsync(request);
+
+            // Assert
+            Assert.IsNotNull(response);
+            Assert.IsTrue(response.Success);
+            A.CallTo(() => repo.UpdateAsync(A<Client>.That.Matches(x => x.IsActive), true)).MustHaveHappenedOnceExactly();
+        }
+    }
+}
